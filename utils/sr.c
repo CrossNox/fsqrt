92f4cf81cb8737cadd3538aaf38b465e9f2f2156
CrossNox <ijmermet@gmail.com>
2016-11-04 09:22:37 -0300

    Update sr.c

--- utils/sr.c
+++ utils/sr.c
@@ -3,7 +3,7 @@
 
 float fsqrt(float n) {
     unsigned i = *(unsigned*)&n;
-    i = (i + 0x3F7B381D) >> 1;
+    i = (i + 0x3F7B381D/*0x3f800000*/) >> 1;
     float y = *(float*)&i;
     y = y*1.5f - n/(2*y); 
     return y;
