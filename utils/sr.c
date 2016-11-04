db99967bd01743e526bfc0022b141527db934da7
CrossNox <javi.m_lml@yahoo.com.ar>
2016-11-04 01:04:03 -0300

    mejorada la constante. debo derivarla bien

--- utils/sr.c
+++ utils/sr.c
@@ -2,21 +2,35 @@
 #include <math.h>
 
 float error_f(float a, float b) {
-    return abs(a-b);
+    return fabsf(a-b);
 }
 
-float fsqrt123(float n) {
+float fsqrt(float n, int delta) {
     unsigned int i = *(int*)&n;
-    i = (i + 0x3f800000) >> 1;
+    i = (i + delta) >> 1;
     float y = *(float*)&i;
     y = y*1.5f - n/(2*y); 
     return y;
 }
 
 int main() {
-    float x = 763.25;
+    float x = 763.26;
     float res = sqrt(x);
-    float res2 = fsqrt123(x);
-    printf("res: %f res2: %f\n",res,res2);
+
+    float min_error = 1.0f;
+    float error_act;
+    int delta_min = 0x3F7B381D;
+    for(int delta = 0x3F7B381D; delta>=0; delta--) {
+        error_act = error_f(res,fsqrt(x,delta));
+        if(error_act<min_error) {
+            //printf("%f error min act\n",error_act);
+           min_error = error_act;
+           delta_min = delta;
+        }            
+        //printf("%f error min act\n",error_act);
+    }
+
+    printf("res: %f res_f:%f\n",res, fsqrt(x,delta_min));
+    printf("min error=%f delta=%d\n",min_error,delta_min);
     return 0;
 }
