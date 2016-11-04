ff3a3e535789b75c9f76a7c6cc0a1fdf3ee3a023
CrossNox <javi.m_lml@yahoo.com.ar>
2016-11-04 00:45:14 -0300

    fixed

--- utils/sr.c
+++ utils/sr.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
 #include <math.h>
 
-#define DELTA 0x3f800000
+float error_f(float a, float b) {
+    return abs(a-b);
+}
 
-float fsqrt(float n) {
-    long i = *(long*)&n;
-    //printf("recibo %f\n",n);
-    i = (i + DELTA)/2;
-    //printf("tengo so far 0x%08x\n",*(int*)&i);
+float fsqrt123(float n) {
+    unsigned int i = *(int*)&n;
+    i = (i + 0x3f800000) >> 1;
     float y = *(float*)&i;
-    //y = y + (y*y-n)/(2*y);
+    y = y*1.5f - n/(2*y); 
     return y;
 }
 
 int main() {
     float x = 763.25;
-    printf("0x%08x\n",*(int*)&x);
     float res = sqrt(x);
-    printf("0x%08x\n",*(int*)&res);
-    float res2 = fsqrt(x);
-    printf("0x%08x\n",*(int*)&res2);
+    float res2 = fsqrt123(x);
     printf("res: %f res2: %f\n",res,res2);
-
     return 0;
 }
