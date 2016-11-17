01e1d2484dfc7731e62c1f6323cb5f7922569f3c
CrossNox <ijmermet@gmail.com>
2016-11-17 22:03:03 +0200

    Update sr.c
    correcciones

--- utils/sr.c
+++ utils/sr.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
 
 float fsqrt(float n) {
     unsigned i = *(unsigned*)&n;
-    i = (i + 0x3F7B381D/*0x3f800000*/) >> 1;
+    i = (i + 0x3f800000) >> 1;
     float y = *(float*)&i;
-    y = y*1.5f - n/(2*y); 
+    y = y*0.5f + n/(2*y); 
+    y = y*0.5f + n/(2*y); 
     return y;
 }
 
 int main() {
-    float x = 763.26;
-    printf("res: %f res_f:%f\n",sqrt(x), fsqrt(x));
+    clock_t t1, t2, diff1, diff2;
+    float res1, res2;
+    float x = 7738563.26;
+    t1 = clock();
+    res1 = fsqrt(x);
+    t2 = clock();
+    diff1 =t2-t1;
+    t1 = clock();
+    res2 = sqrt(x);
+    t2 = clock();
+    diff2 = t2 - t1;
+    printf("fsqrt:\tresultado:%f\tciclos:%ld\n",res1,(long)diff1);
     return 0;
 }
