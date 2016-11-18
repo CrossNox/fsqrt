85b7684afb8b7cb873336b447c3bebe94b0f8cf7
CrossNox <javi.m_lml@yahoo.com.ar>
2016-11-18 01:09:48 -0300

    pruebas de tiempo

--- utils/sr.c
+++ utils/sr.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
+#include <stdlib.h>
 
 float fsqrt(float n) {
     unsigned i = *(unsigned*)&n;
     i = (i + 0x3f800000) >> 1;
     float y = *(float*)&i;
-    y = y*0.5f + n/(2*y); 
-    y = y*0.5f + n/(2*y); 
+    y = y*0.5f + n/(2*y);  
     return y;
 }
 
 int main() {
-    clock_t t1, t2, diff1, diff2;
-    float res1, res2;
-    float x = 7738563.26;
-    t1 = clock();
-    res1 = fsqrt(x);
-    t2 = clock();
-    diff1 =t2-t1;
-    t1 = clock();
-    res2 = sqrt(x);
-    t2 = clock();
-    diff2 = t2 - t1;
-    printf("fsqrt:\tresultado:%f\tciclos:%ld\n",res1,(long)diff1);
-    printf("sqrt:\tresultado:%f\tciclos:%ld\n",res2,(long)diff2);
+    clock_t t1, t2, diff1=0, diff2=0;
+    float res1, res2, x;
+    for(size_t i= 0; i < 100000 ; i++) {
+        x = ((float)rand())/1000;        
+        t1 = clock();
+        res2 = sqrt(x);
+        t2 = clock();
+        diff2 += t2-t1;
+        t1 = clock();
+        res1 = fsqrt(x);
+        t2 = clock();
+        diff1 += t2-t1;
+        printf("x:%f\tsqrt:%f\tfsqrt:%f\n",x,res2,res1);
+    }
+    printf("%ld sqrt \t%ld fsqrt\n",diff2,diff1);
     return 0;
 }
