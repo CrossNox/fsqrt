97e008972c359533e8ea35528e104abe807faec3
CrossNox <ijmermet@gmail.com>
2016-11-21 13:32:27 -0300

    Update sr.c
    minor fixes

--- utils/sr.c
+++ utils/sr.c
@@ -12,12 +12,27 @@ float fsqrt(float n) {
     return y;
 }
 
+int main() {#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
+
+float fsqrt(float n) {
+    unsigned i = *(unsigned*)&n;
+    i = (i + 0x3f800000) >> 1;
+    float y = *(float*)&i;
+    y = y*0.5 + n/(2*y);
+    y = y*0.5 + n/(2*y);  
+    return y;
+}
+
 int main() {
     srand(clock());
     clock_t t1, t2, diff1=0, diff2=0;
     float res1, res2, x;
-    for(size_t i= 0; i < 100000 ; i++) {
-        x = ((float)rand())/1000;        
+    #define SHITLOAD 100000
+    for(size_t i= 0; i < SHITLOAD ; i++) {
+        x = ((float)rand())/10000;        
         t1 = clock();
         res2 = sqrt(x);
         t2 = clock();
@@ -26,22 +41,22 @@ int main() {
         res1 = fsqrt(x);
         t2 = clock();
         diff1 += t2-t1;
-        printf("x:%.4f\tsqrt:%.6f\tfsqrt:%.6f\n",x,res2,res1);
+        printf("x:%.6f\tsqrt:%.6f\tfsqrt:%.6f\n",x,res2,res1);
     }
     printf("%ld sqrt \t%ld fsqrt\n",diff2,diff1);
 
     //Let's use it to calculate pi
-    size_t hits = 0;
-    #define SHITLOAD 100000000
+    size_t hits = 0;    
     for(size_t darts = 0; darts < SHITLOAD ; darts++) {
-        float dart_x = -0.5f+(float)((double)rand()/(double)(RAND_MAX));
-        float dart_y = -0.5f+(float)((double)rand()/(double)(RAND_MAX));
-        if(fsqrt(dart_y*dart_y+dart_x*dart_x)<=0.5f)
+        float dart_x = -1.0f+(float)(((double)rand()/(double)(RAND_MAX))*2);
+        float dart_y = -1.0f+(float)(((double)rand()/(double)(RAND_MAX))*2);
+        if(fsqrt(dart_y*dart_y+dart_x*dart_x)<=1.0f)
             hits++;
     }
     printf("%zu hits\n",hits);
     float pi_aprox = (float)hits/SHITLOAD;
-    pi_aprox *= 4.0f;
+    pi_aprox *= 4.0f; //Why this, you may ask. Well, this code is full of
+    //magic numbers, so deal with it.
     printf("%f aprox pi\n",pi_aprox);
     return 0;
 }
